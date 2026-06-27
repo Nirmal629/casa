17f336b5f3c271eb77ae4ee9631b2bfffdb25a80
nirmal629 <nir.multi2018@gmail.com>
2026-06-28 00:53:58 +0530

    Stop tracking local DB connection files

--- dbConnection_PDO.php
+++
@@ -1,7 +0,0 @@
-<?php
-            // 1. Database Configuration (Adjust if your connection file is already included)
-            $host    = 'localhost';
-            $db      = 'casa_test';
-            $user    = 'casa_test';
-            $pass    = 'casa_test123#';
-            $charset = 'utf8mb4';
